b1206c06fd83d19b9550f52120c451a5a215450b
umangparekh001 <umang.m.parekh@gmail.com>
2015-12-14 02:40:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
@@ -15,7 +17,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
 
     // ejs render automatically looks in the views folder
-    res.render('./app/index');
+    res.render('app/index.html');
 });
 
 app.listen(port, function() {
